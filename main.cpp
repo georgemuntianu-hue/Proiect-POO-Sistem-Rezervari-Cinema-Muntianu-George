21d580e3b8c060ca4b3bdcd766c1f956775237d7
georgemuntianu-hue <george.muntianu@student.usv.ro>
2026-05-03 18:30:36 +0300

    Refactor cinema reservation system and improve prompts

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include <string>
 #include "Loc.h"
 #include "Bilet.h"
+#include "Film.h"
 
 using namespace std;
 
+// Functie pentru afisarea hartii salii
 void afisareSala(const vector<Loc>& sala) {
     cout << "\n      --- ECRAN CINEMA ---\n\n";
     cout << "        1  2  3  4  5  6  7  8  9  10 (Locuri)\n";
@@ -14,6 +16,7 @@ void afisareSala(const vector<Loc>& sala) {
     for (size_t i = 0; i < sala.size(); i++) {
         if (sala[i].status()) cout << "XX ";
         else cout << "L  ";
+        
         if ((i + 1) % 10 == 0 && (i + 1) < sala.size()) {
             randCurent++;
             cout << "\nR. " << randCurent << " | ";
@@ -23,6 +26,20 @@ void afisareSala(const vector<Loc>& sala) {
 }
 
 int main() {
+    string numeFilm;
+    int tipFormat;
+    bool este3D;
+
+    // Pasul 1: Configurarea filmului
+    cout << "Introduceti numele filmului: ";
+    getline(cin >> ws, numeFilm); 
+    cout << "Alegeti formatul (1 pentru 3D, 0 pentru 2D): ";
+    cin >> tipFormat;
+    este3D = (tipFormat == 1);
+
+    Film filmCurent(numeFilm, este3D);
+
+    // Pasul 2: Initializarea salii (3 randuri x 10 locuri)
     vector<Loc> cinema;
     for (int r = 1; r <= 3; r++) {
         for (int l = 1; l <= 10; l++) {
@@ -31,7 +48,7 @@ int main() {
     }
 
     string numeZi;
-    cout << "Introduceti ziua saptamanii (ex: Luni, Vineri, sambata): ";
+    cout << "Introduceti ziua saptamanii (ex: luni, vineri): ";
     cin >> numeZi;
 
     int optiune;
@@ -39,32 +56,37 @@ int main() {
         cout << "\n1. Sala | 2. Rezerva | 0. Iesire\nOptiune: ";
         cin >> optiune;
 
-        if (optiune == 2) {
+        if (optiune == 1) {
+            afisareSala(cinema);
+        } 
+        else if (optiune == 2) {
             int rDorit, lDorit;
             cout << "Rand (1-3): "; cin >> rDorit;
             cout << "Loc (1-10): "; cin >> lDorit;
 
+            // Calcul index pentru vectorul de tip Loc
             int index = (rDorit - 1) * 10 + (lDorit - 1);
 
             if (rDorit < 1 || rDorit > 3 || lDorit < 1 || lDorit > 10) {
-                cout << "Loc inexistat!\n";
-            } else if (cinema[index].status()) {
+                cout << "Loc inexistent!\n";
+            } 
+            else if (cinema[index].status()) {
                 cout << "Locul este deja ocupat!\n";
-            } else {
-                int tip;
-                cout << "Categorie: 1.Adult 2.Elev 3.Copil 4.Dizabilitati: ";
-                cin >> tip;
+            } 
+            else {
+                int tipP;
+                cout << "Categorie (1.Adult 2.Elev 3.Copil 4.Dizab): ";
+                cin >> tipP;
 
-                Bilet b(tip, numeZi);
+                // Creare obiect bilet si marcare loc ca ocupat
+                Bilet b(tipP, numeZi);
                 cinema[index].ocupaLoc();
 
-                cout << "\n--- BILET GENERAT ---" << endl;
-                cout << "Zi: " << numeZi << " | Categorie: " << b.getNumeTip() << endl;
-                cout << "Locatie: R" << rDorit << " L" << lDorit << endl;
-                cout << "PRET: " << b.calculeazaPret() << " RON" << endl;
+                cout << "\n--- BILET REZERVAT ---\n";
+                cout << "Film: " << filmCurent.titlu << (filmCurent.este3D ? " (3D)" : " (2D)") << endl;
+                cout << "Locatie: Rand " << rDorit << ", Loc " << lDorit << endl;
+                cout << "Pret: " << b.calculeazaPret(filmCurent.este3D) << " RON" << endl;
             }
-        } else if (optiune == 1) {
-            afisareSala(cinema);
         }
     } while (optiune != 0);
 
